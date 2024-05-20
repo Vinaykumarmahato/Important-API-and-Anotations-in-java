8cb99454c37be8200aecd8b7dfaf193935cb02e1
Vinay kumar <vinaykumar860964@gmail.com>
2024-05-20 13:21:16 +0530

    Updated

---
+++ 📅 Date and Time API/Solution1.java
@@ -0,0 +1,12 @@
+// import java.util.Date;
+public class Solution1 {
+
+    public static void main(String[] args) {
+
+    java.util.Date dt=new java.util.Date(); // this is also a way of importing the date package.
+    // java.util.ArrayList a=new java.util.ArrayList();
+    System.out.println(dt);
+
+        
+    }
+}
